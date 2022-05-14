03e469ecffe5bbde439385e715dc429124771600
Eder Sena <eder.sena@live.com>
2022-05-14 17:57:33 -0300

    Update ZIndex.vue

--- tailwind/src/views/ZIndex.vue
+++ tailwind/src/views/ZIndex.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container relative bg-purple-400 border-2 border-purple-900 m-4 border-box p-10">
-    <div class="bg-red-300 h-20 w-20 p-6 absolute z-10">Na frente</div>
-    <div class="bg-red-900 h-20 w-20 p-6 absolute m-1">Atrás</div>
+    <div class="bg-red-300 h-20 w-20 p-6 absolute z-20">Atrás</div>
+    <div class="bg-red-900 h-20 w-20 p-6 absolute z-10 m-5">Meio</div>
+    <div class="bg-blue-900 text-white h-20 w-20 p-6 absolute m-10">Frente</div>
   </div>
 </template>
