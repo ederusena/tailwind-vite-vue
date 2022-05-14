2d10512abcf07fd6dbc1999cc62bce80f8b018ff
Eder Sena <eder.sena@live.com>
2022-05-14 00:12:32 -0300

    :rocket: tailwind fundamentals

---
+++ tailwind/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded">Tudo</button>
+  <div class="w-4 md:w-6 h-4 md:h-6 m-4 bg-green-400 md:bg-green-700"></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
