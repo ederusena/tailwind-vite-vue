a57eee0efe8b1198a4cc835253b53f143f0a56a1
Eder Sena <eder.sena@live.com>
2022-05-14 13:31:15 -0300

    :recycle: Exercicio

--- tailwind/src/views/PositionDirection.vue
+++ tailwind/src/views/PositionDirection.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="container bg-purple-400 border-2 border-purple-900 m-4 border-box p-10">
-    <p>Border box</p>
+  <div class="relative bg-pink-500 m-10">
+    TESTE
+    <div class="absolute top-0 text-white right-0 bg-pink-900 p-1">
+      Absolute
+    </div>
+  </div>
+
+  <div class="container relative h-40 bg-lime-200 m-10">
+    <div class="absolute right-0 bottom-0 h-5 w-5 bg-red-600"></div>
+    <div class="absolute right-0 top-0 h-5 w-5 bg-red-600"></div>
+    <div class="absolute left-0 bottom-0 h-5 w-5 bg-red-600"></div>
+    <div class="absolute left-0 top-0 h-5 w-5 bg-red-600"></div>
   </div>
 </template>
